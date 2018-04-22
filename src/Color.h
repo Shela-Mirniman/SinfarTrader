360fb0583cb5d6b124cfcc8c1260514544ad13a9
Pablo Strasser <strasserpablo@bluewin.ch>
2018-04-23 00:06:29 +0200

    Improved color.

--- src/Color.h
+++ src/Color.h
@@ -1,22 +1,31 @@
 #pragma once
 #include <string>
 #include <functional>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/categories.hpp> 
+#include <boost/iostreams/code_converter.hpp>
+#include <boost/locale.hpp>
 
-inline int ToPrintableASCII(int value)
+inline std::string ToPrintableASCII(char value)
 {
+    std::string res;
     if(value<32)
     {
-	return 32;
+        res+=char(32);
     }
     else if(value>254)
     {
-	return 254;
+        res+=char(254);
     }
-    return value;
+    else
+    {
+        res+=value;
+    }
+    return boost::locale::conv::to_utf<char>(res,"Latin1");
 }
 inline std::string ColorText(int red,int green,int blue,std::string text)
 {
-    return std::string("<c")+char(ToPrintableASCII(red))+char(ToPrintableASCII(green))+char(ToPrintableASCII(blue))+std::string(">")+text+std::string("</c>")+char(ToPrintableASCII(red))+char(ToPrintableASCII(green))+char(ToPrintableASCII(blue));                                                                     
+    return std::string("<c")+ToPrintableASCII(red)+ToPrintableASCII(green)+ToPrintableASCII(blue)+std::string(">")+text+std::string("</c>");                                                                     
 }
 
 static auto ColorRed=std::bind(ColorText,231,32,32,std::placeholders::_1);
