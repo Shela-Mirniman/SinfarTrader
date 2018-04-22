b2da32fcb9e48d0574b369f92ca5a3b415bb2530
Pablo Strasser <strasserpablo@bluewin.ch>
2018-04-22 15:04:35 +0200

    Added count for AddAccount.

--- src/RessourcesManager.cpp
+++ src/RessourcesManager.cpp
@@ -231,6 +231,7 @@ void RessourcesManager::AddAccount(std::function<void(std::string)> func,int PCI
         {
             char *errorMessage;
             std::string sql=std::string("UPDATE account set PlayerId=")+std::to_string(PlayerId)+std::string(" , PlayerName='")+PlayerName+std::string("' , name='")+Escape(name)+std::string("' , permission_level=")+std::to_string(permission_level)+("' , fee=")+std::to_string(fee)+std::to_string(permission_level)+std::string(" where PCId=")+std::to_string(PCId);
+            std::cout<<"sql "<<sql<<std::endl;
             if(sqlite3_exec(m_database->m_sqlite,sql.c_str(),nullptr,nullptr,&errorMessage)!=SQLITE_OK)
             {
                 throw std::runtime_error(errorMessage);
@@ -240,6 +241,7 @@ void RessourcesManager::AddAccount(std::function<void(std::string)> func,int PCI
         {
             char *errorMessage;
             std::string sql=std::string("INSERT INTO account VALUES(")+std::to_string(PCId)+std::string(",")+std::to_string(PlayerId)+std::string(",'")+PlayerName+std::string("','")+Escape(name)+std::string("',")       +std::to_string(permission_level)+std::string(",")+std::to_string(fee)+std::string(")");
+            std::cout<<"sql "<<sql<<std::endl;
             if(sqlite3_exec(m_database->m_sqlite,sql.c_str(),nullptr,nullptr,&errorMessage)!=SQLITE_OK)
             {
                 throw std::runtime_error(errorMessage);
