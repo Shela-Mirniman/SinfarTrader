874cbafb59788284d3ebd04bcc0d15dee5ccf793
Pablo Strasser <strasserpablo@bluewin.ch>
2018-04-22 23:17:56 +0200

    Changed the red color.

--- src/Color.h
+++ src/Color.h
@@ -8,9 +8,9 @@ inline int ToPrintableASCII(int value)
     {
 	return 32;
     }
-    else if(value>127)
+    else if(value>254)
     {
-	return 127;
+	return 254;
     }
     return value;
 }
@@ -19,7 +19,7 @@ inline std::string ColorText(int red,int green,int blue,std::string text)
     return std::string("<c")+char(ToPrintableASCII(red))+char(ToPrintableASCII(green))+char(ToPrintableASCII(blue))+std::string(">")+text+std::string("</c>");                                                                     
 }
 
-static auto ColorRed=std::bind(ColorText,255,0,0,std::placeholders::_1);
+static auto ColorRed=std::bind(ColorText,231,32,32,std::placeholders::_1);
 static auto ColorGreen=std::bind(ColorText,0,255,0,std::placeholders::_1);
 static auto ColorBlue=std::bind(ColorText,0,0,255,std::placeholders::_1);
 static auto ColorPurple=std::bind(ColorText,128,0,128,std::placeholders::_1);
