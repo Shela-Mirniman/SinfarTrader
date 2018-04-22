c74aab3f1992c13a96df05ba305597b63642c3f3
Pablo Strasser <strasserpablo@bluewin.ch>
2018-04-23 01:11:01 +0200

    Red Color now work.

--- src/Color.h
+++ src/Color.h
@@ -6,12 +6,16 @@
 #include <boost/iostreams/code_converter.hpp>
 #include <boost/locale.hpp>
 
-inline std::string ToPrintableASCII(char value)
+/*inline std::string ToPrintableASCII(char value)
 {
     std::string res;
     if(value<32)
     {
         res+=char(32);
+    }
+    else if(value>126&&value<=254)
+    {
+	res+=(value+33);
     }
     else if(value>254)
     {
@@ -21,14 +25,21 @@ inline std::string ToPrintableASCII(char value)
     {
         res+=value;
     }
-    return boost::locale::conv::to_utf<char>(res,"Latin1");
-}
-inline std::string ColorText(int red,int green,int blue,std::string text)
-{
-    return std::string("<c")+ToPrintableASCII(red)+ToPrintableASCII(green)+ToPrintableASCII(blue)+std::string(">")+text+std::string("</c>");                                                                     
+    return res; 
 }
 
-static auto ColorRed=std::bind(ColorText,231,32,32,std::placeholders::_1);
+inline std::string ColorText(int red,int green,int blue,std::string text)
+{
+    return std::string("<c")+ToPrintableASCII(red)+ToPrintableASCII(green)+ToPrintableASCII(blue)+std::string(">")+text+std::string("</c>")+ToPrintableASCII(red)+ToPrintableASCII(green)+ToPrintableASCII(blue);
+}
+*/
+
+inline std::string ColorRed(std::string text)
+{
+    return std::string("<c´  >")+text+std::string("</c>");
+}
+/*
 static auto ColorGreen=std::bind(ColorText,0,255,0,std::placeholders::_1);
 static auto ColorBlue=std::bind(ColorText,0,0,255,std::placeholders::_1);
 static auto ColorPurple=std::bind(ColorText,128,0,128,std::placeholders::_1);
+*/
